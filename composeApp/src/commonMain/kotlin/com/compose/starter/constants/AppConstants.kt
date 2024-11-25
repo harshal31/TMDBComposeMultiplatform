9b238fcbcd8ca713fe6956303f4d75ad0784ecc1
Harshal <harshal.chaudhari3131@gmail.com>
2024-11-25 20:04:14 +0530

    Update AppConstants.kt

--- composeApp/src/commonMain/kotlin/com/compose/starter/constants/AppConstants.kt
+++ composeApp/src/commonMain/kotlin/com/compose/starter/constants/AppConstants.kt
@@ -2,8 +2,7 @@ package com.compose.starter.constants
 
 object AppConstants {
 
-    const val NET_KEY =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTkwOTliMWJjOGY1NzVkNmU2YTgzMTkzNWU4ZGMxNSIsIm5iZiI6MTcyNDMyNzM1OS42MzIyMjgsInN1YiI6IjVlNjFmYmQ1MjJhZjNlMDAxM2RjZGU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._nT5Fs3ORccnc4PlrtwpeGsVQ8q7r9zAxX540c1BSuE"
+    const val NET_KEY = ""
 
     const val FULL_SIZE_BASE_URL = "https://image.tmdb.org/t/p/original"
     const val CROP_SIZE_BASE_URL = "https://image.tmdb.org/t/p/w500"
